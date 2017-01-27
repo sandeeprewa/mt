e273bdab3a1c8901ace490e9bb7ff8eaa4c33626
sandeep <91anu.sandeep@gmail.com>
2017-01-27 10:33:10 +0530

    initial commit for default project set up

---
+++ mt/src/main/java/com/mt/web/controller/HibernateController.java
@@ -0,0 +1,16 @@
+package com.mt.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HibernateController {
+
+	@RequestMapping(value="/test")
+	@ResponseBody
+	public String testRelationship(){
+	return "testing : controller is working";
+	}
+	
+}
